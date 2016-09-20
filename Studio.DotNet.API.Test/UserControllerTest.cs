b0122fa310fc966b064c556843408ba33223bdec
Sinx He <HeabKing@qq.com>
2016-09-20 14:59:12 +0800

    add test for user post

--- Studio.DotNet.API.Test/UserControllerTest.cs
+++ Studio.DotNet.API.Test/UserControllerTest.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using Sinx.Utility.Extension;
 using Xunit;
 
 namespace Studio.DotNet.API.Test
@@ -15,7 +18,19 @@ namespace Studio.DotNet.API.Test
         [Fact]
         public void PostAsyncTest()
         {
-            // TODO
+            string reqeustString = @"
+                POST http://localhost:6688/api/user HTTP/1.1
+                Host: localhost:6688
+                Content-Length: 42
+                Content-Type: application/json; charset=utf-8
+
+                {""email"":""test@test.com"",""password"":""123123""}";
+            var response = new HttpClient().SendAsync(HttpRequestMessageEx.CreateFromRaw(reqeustString)).Result;
+            var content = response.Content.ReadAsStringAsync().Result;
+            var userid = Convert.ToInt32(Regex.Match(content, @"id.+?(?<userid>\d+)").Result("${userid}"));
+            Assert.True(response.StatusCode == System.Net.HttpStatusCode.Created);
+            Assert.True(Regex.IsMatch(content, "id", RegexOptions.IgnoreCase));
+            Assert.True(userid > 0);
         }
     }
 }
